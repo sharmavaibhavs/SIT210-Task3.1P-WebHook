5030e4e91fcc5ee1e9ae1f99e773e4b69126c91d
sharmavaibhavs <72638474+sharmavaibhavs@users.noreply.github.com>
2021-05-01 15:41:12 +1000

    Add files via upload

---
+++ 3p.ino
@@ -0,0 +1,17 @@
+int led = D7;
+
+void setup() {
+    pinMode(led, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(led, HIGH);
+    
+    String temp = String(random(60, 80));
+    Particle.publish("temp", temp, PRIVATE);
+    delay(30000);
+    
+    digitalWrite(led, LOW);
+    delay(30000);
+    
+}
